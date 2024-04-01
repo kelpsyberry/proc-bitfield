6b875dcf1abf1b89573a42227b373017be72b4f7
kelpsyberry <138107494+kelpsyberry@users.noreply.github.com>
2024-04-01 19:35:54 +0200

    Run rustfmt

--- src/conv.rs
+++ src/conv.rs
@@ -5,7 +5,7 @@ mod impls;
 /// Equivalent of [`core::ops::Try`] that doesn't require nightly and allows changing the output
 /// type using a GAT (logically depends on [`core::ops::Try`], but doesn't have an explicit
 /// dependency as it's unstable).
-/// 
+///
 /// Automatically implemented for Option<T> and Result<T, E>.
 pub trait Try {
     type Output;
